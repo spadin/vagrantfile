4ad9378a8eb283c9e4fb8dc43642516cdb8ecb1a
Sandro Padin <sandropadin@gmail.com>
2016-03-30 13:13:42 -0500

    Updating to use vim-plug

--- scripts/install_homeshick.sh
+++ scripts/install_homeshick.sh
@@ -4,10 +4,10 @@ git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 
 homeshick clone jimeh/git-aware-prompt
-homeshick clone gmarik/vundle.vim
+homeshick clone junegunn/vim-plug
 homeshick clone spadin/dotbashrc
 homeshick clone spadin/dotvimrc
 homeshick clone spadin/dotfilesdeux
 homeshick link -f
 
-vim -u ~/.vimrc.d/plugins.vim +PluginInstall +qall
+vim -u ~/.vimrc.d/plugins.vim +PlugInstall +qall
